f85a14ddb7af0a2e6c4a428aaf996f583d3fefd8
Michal Borejszo <michael.borejszo@gmail.com>
2022-11-16 00:28:46 +0100

    not needed error

--- src/errors.rs
+++ src/errors.rs
@@ -8,9 +8,6 @@ pub enum BitcoinMessageError {
     #[error("command name has to be ASCII string")]
     CommandNameNonAscii,
 
-    #[error("unknown service identifier: {0}")]
-    ServiceIdentifierUnknown(u64),
-
     #[error("IO Error during (de)serialization: {0}")]
     SerializationError(#[from] std::io::Error),
 
